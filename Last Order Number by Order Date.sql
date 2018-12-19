1f77264d7b9516bf8a63fb7cc59c019f1208d26a
fnjohnny <45018314+fnjohnny@users.noreply.github.com>
2018-12-19 13:17:33 -0800

    Create Last Order Number by Order Date.sql

---
+++ Last Order Number by Order Date.sql
@@ -0,0 +1,6 @@
+SELECT MAX(order_number)
+FROM t_order
+WHERE CAST(order_date As DATE) = '2018-11-26'
+AND order_number NOT LIKE 'TO%'
+AND order_number NOT LIKE 'RTV%'
+AND order_number NOT LIKE 'SO%'
