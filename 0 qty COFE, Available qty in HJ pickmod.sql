32401517f14c673fa5935a77b18bebcd1bc6be63
zedrickb <45638203+zedrickb@users.noreply.github.com>
2018-12-19 18:58:14 -0800

    Update and rename 0 qty COFE, 5+ qty in HJ pickmod.sql to 0 qty COFE, Available qty in HJ pickmod.sql

--- 0 qty COFE, Available qty in HJ pickmod.sql
+++ 0 qty COFE, Available qty in HJ pickmod.sql
@@ -1,6 +1,6 @@
 SELECT location_id, hu_id, item_number, actual_qty, status FROM t_stored_item with(NOLOCK)
 WHERE location_id LIKE 'A%'
 AND status = 'A'
-AND actual_qty > '5'
-AND item_number IN ()
+AND actual_qty > '0'
+AND item_number IN (--insert cofe sku shortage--)
 GROUP BY location_id, hu_id, item_number, actual_qty, status
